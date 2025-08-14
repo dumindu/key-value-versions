8546584681c72a12bbcea6101d380951fdb5c08a
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2025-08-14 09:01:31 +0800

    Fix Api/V1/AuthController

--- key-value-versions-app/app/Http/Controllers/Api/V1/AuthController.php
+++ key-value-versions-app/app/Http/Controllers/Api/V1/AuthController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\Rules;
 
 class AuthController extends Controller
@@ -29,56 +28,6 @@ class AuthController extends Controller
         ]);
 
         return response()->json(['message' => 'User registered successfully'], 201);
-    }
-
-    public function login(Request $request): JsonResponse
-    {
-        $request->validate([
-            'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'string'],
-        ]);
-
-        if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(['message' => 'Invalid credentials'], 401);
-        }
-
-        $response = Http::asForm()->post(env('APP_URL') . '/oauth/token', [
-            'grant_type' => 'password',
-            'client_id' => env('PASSPORT_PASSWORD_CLIENT_ID'),
-            'client_secret' => env('PASSPORT_PASSWORD_SECRET'),
-            'username' => $request->email,
-            'password' => $request->password,
-            'scope' => '*',
-        ]);
-
-        return response()->json([
-            'user' => Auth::user(),
-            'token' => $response->json(),
-        ]);
-    }
-
-    public function me(): JsonResponse
-    {
-        $user = auth()->user();
-
-        return response()->json(['user' => $user]);
-    }
-
-    public function refreshToken(Request $request): JsonResponse
-    {
-        $formData = $request->validate([
-            'refresh_token' => ['required', 'string'],
-        ]);
-
-        $response = Http::asForm()->post(env('APP_URL') . '/oauth/token', [
-            'grant_type' => 'refresh_token',
-            'refresh_token' => $formData->refresh_token,
-            'client_id' => env('PASSPORT_PASSWORD_CLIENT_ID'),
-            'client_secret' => env('PASSPORT_PASSWORD_SECRET'),
-            'scope' => '*',
-        ]);
-
-        return response()->json(['token' => $response->json()]);
     }
 
     public function logout(): JsonResponse
--- key-value-versions-app/routes/api.php
+++ key-value-versions-app/routes/api.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/refresh', [AuthController::class, 'refreshToken']);
-
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => ['auth:api']], function () {
